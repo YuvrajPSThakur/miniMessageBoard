087dd3d05fdf207a9f2feee247e7d8aaaee51dad
YuvrajPSThakur <ythakur@ec.iitr.ac.in>
2021-09-01 18:23:38 +0530

    first commit

---
+++ Messages.js
@@ -0,0 +1,22 @@
+const messages = [
+    { 
+        id: 1,
+        text: "Hello World!",
+        user: "User-1",
+        added: new Date().toLocaleString()
+    },
+    {
+        id: 2,
+        text: "Testing node",
+        user: "User-2",
+        added: new Date().toLocaleString()
+    },
+    {
+        id: 3,
+        text: "The purpose of our lives is to be happy.",
+        user: "User-3",
+        added: new Date().toLocaleString()
+    }
+ ];
+
+ module.exports = messages;
